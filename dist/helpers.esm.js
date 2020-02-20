3b0ba1eb691a8113e578947cc3a0c037d5f904bb
jt <huangjiangtao@hexdo.com>
2020-02-20 18:06:26 +0800

    release: publish

---
+++ dist/helpers.esm.js
@@ -0,0 +1,20 @@
+/*!
+ * helpers v0.1.0
+ * (c) 2020 huangjiangtao@hexdo.com
+ * Released under the BSD 3-Clause License.
+ */
+
+const detector = require('./detector');
+
+const rules = require('./validators/rule');
+
+const Validator = require('./validators/Validator');
+
+const controlCompiler = require('./compiler/control');
+
+module.exports = {
+  detector,
+  rules,
+  Validator,
+  controlCompiler
+};
---
+++ dist/helpers.js
@@ -0,0 +1,27 @@
+/*!
+ * helpers v0.1.0
+ * (c) 2020 huangjiangtao@hexdo.com
+ * Released under the BSD 3-Clause License.
+ */
+
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+}((function () { 'use strict';
+
+  const detector = require('./detector');
+
+  const rules = require('./validators/rule');
+
+  const Validator = require('./validators/Validator');
+
+  const controlCompiler = require('./compiler/control');
+
+  module.exports = {
+    detector,
+    rules,
+    Validator,
+    controlCompiler
+  };
+
+})));
